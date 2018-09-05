738e1128d20398d1a63292f329d5c7df1dbaff9c
seonghun <shun6889@emgram.kr>
2018-09-05 17:15:34 +0900

    New commit message

--- app/src/main/java/com/example/emgram/jenkinstest/JTApplication.java
+++ app/src/main/java/com/example/emgram/jenkinstest/JTApplication.java
@@ -3,4 +3,8 @@ package com.example.emgram.jenkinstest;
 import android.app.Application;
 
 public class JTApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
